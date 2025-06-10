3868b94fe22705a1a2b80fff71e0f5b10e0b5e2b
Hazem Belal <hazembelal288@gmail.com>
2025-06-10 22:10:35 +0300

    git commit -m "Add build/ to .gitignore"

--- devel/share/ma_rrt_path_plan/cmake/ma_rrt_path_plan-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(ma_rrt_path_plan_MESSAGE_FILES "/home/hazem/Downloads/project/src/ma_rrt_path_plan/msg/Waypoint.msg;/home/hazem/Downloads/project/src/ma_rrt_path_plan/msg/WaypointsArray.msg;/home/hazem/Downloads/project/src/ma_rrt_path_plan/msg/Map.msg")
-set(ma_rrt_path_plan_SERVICE_FILES "")
